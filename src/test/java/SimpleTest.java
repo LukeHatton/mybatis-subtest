0b76a07f06adcc9d6c703ddc7a908801accfc08b
lizhao <lukehatton685@gmail.com>
2021-12-09 22:22:32 +0800

    随意测试
    测试了时间类的处理，了解了mac系统对时间的显示逻辑

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,5 +1,6 @@
 import com.pojo.People;
 import lombok.Cleanup;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -7,13 +8,23 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
+import java.util.function.Function;
 
 /**
  * <p>project: untitled
@@ -62,6 +73,43 @@ public class SimpleTest {
         //工厂生产SqlSession对象
         @Cleanup SqlSession sqlSession = build.openSession();
         People people = sqlSession.selectOne("com.mapper.PeopleMapper.selectByPrimaryKey", 1);
-        log.info(people.toString());
+        log.info("==> {}", people.toString());
+    }
+
+    /**
+     * 自定义函数接口，在方法声明中抛出异常，免掉在函数定义中处理异常的痛苦
+     *
+     * @param <T> 函数入参
+     * @param <R> 函数出参
+     */
+    @FunctionalInterface
+    interface CheckedFunction<T, R> {
+        R apply(T t) throws Throwable;
+    }
+
+    /**
+     * 测试时间对象，发现mac系统小坑：24小时制，中午12点后显示是12 pm，凌晨0点后显示是12 am
+     */
+    @SneakyThrows
+    @Test
+    public void test03() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //获取文件的创建时间
+        CheckedFunction<File, Date> getCreateDateTime = file -> {
+            FileTime creationTime = (FileTime) Files.getAttribute(file.toPath(), "creationTime");
+            return Date.from(creationTime.toInstant());
+        };
+        //将时间转换成对应格式的字符串
+        Function<Date, String> dateStringFunction = date -> dateTimeFormatter.format(date.toInstant()
+                .atZone(ZoneId.systemDefault()));
+        // File file = new File("/Users/lizhao/Downloads/helloWorld.js");
+        File file = new File("/Users/lizhao/Downloads/必应/蓝山公园的萤火虫-澳大利亚.jpg");
+        Date date = getCreateDateTime.apply(file);
+        System.out.println(format.format(date));
+        System.out.println(dateStringFunction.apply(date));
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        LocalDateTime minusHours = localDateTime.minusHours(2L);
+        System.out.println(dateTimeFormatter.format(minusHours));
     }
 }
